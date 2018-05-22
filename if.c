d78a71e9210fc993b13d04f6e86a86cb1cdf6e7d
CYSY-CYH <rp_eaflychan@163.com>
2018-05-22 10:37:33 +0800

    if-else
    if-else

---
+++ if.c
@@ -0,0 +1,81 @@
+#include<stdio.h>
+
+void main()
+{
+	char a1;
+	scanf("%s",&a1);
+	printf("a1=%s",a1);
+//	return 0;	
+}
+
+//============1=============
+//int main()
+//{
+//	int a1=0;
+//	scanf("%d",&a1);
+//	if(a1==9)
+//		printf("good job!");
+//	else
+//		printf("error number");	
+//}
+
+//============2=============
+//int main()
+//{
+//	int a1=0;
+//	scanf("%d",&a1);
+//	if(a1==1)
+//		printf("this is one!");
+//	else if(a1==2)
+//		printf("this is two!");
+//	else
+//		printf("this is not in scope!");			
+//}
+
+//============3=============
+//int main()
+//{
+//	int a1=0,a2=0;
+//	scanf("%d%d",&a1,&a2);
+//	printf("a1=%d,a2=%d\n",a1,a2);
+//	if(a1==1)
+//	  if(a2==2)
+//			printf("now in second floor!");
+//		else
+//			printf("now in first floor!");
+//	else
+//		printf("game over!");
+//}
+
+//============4=============
+//int main()
+//{
+//	int a1=0,a2=0;
+//	scanf("%d",&a1);
+//	printf("a1=%d\n",a1);
+//	if(a1==1)
+//	{
+//		printf("this is first floor!\n");
+//		scanf("%d",&a2);
+//		printf("a2=%d\n",a2);
+//	  if(a2==2)
+//			printf("this is second floor!");
+//		else
+//			printf("game over!");
+//	}	
+//	else
+//		printf("this is not in scope!");
+//}
+
+//============5=============
+//int main()
+//{
+//	int a1=0,a2=0;
+//	scanf("%d%d",&a1,&a2);
+//	printf("a1=%d,a2=%d\n",a1,a2);
+//	if(a1==1 && a2==2)
+//		printf("bravo!");
+//	else
+//		printf("sorry,game over!");
+//}
+
